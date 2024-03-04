fca550716e08f34593a99cd9d6759e26411e6cdf
Nicole Mongan <97134387+NikiM95@users.noreply.github.com>
2024-03-04 13:35:46 +0000

    Add files via upload

---
+++ BooleanLogicAssignment/BooleanLogicAssignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogicAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI violation? Please answer 'yes or no'.");
+            string DUI = Console.ReadLine();
+
+            Console.WriteLine("How many speeding tickets have you had?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+            bool answer = (age > 15 && DUI == "no" && tickets <= 3);
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(answer);
+            Console.ReadLine();
+            
+
+
+
+        }
+    }
+}
