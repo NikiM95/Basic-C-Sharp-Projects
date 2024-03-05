37e469943c994b1c899fe5fdc29d51cf1436b5d4
Nicole Mongan <97134387+NikiM95@users.noreply.github.com>
2024-03-05 14:06:58 +0000

    Add files via upload

---
+++ LoopAssignment/LoopAssignment/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Who is the eldest sister? Nicole, Shauna or Shannon:");
+            string eldest = Console.ReadLine();
+            bool isGuessed = eldest == "Shauna";
+
+            do
+            {
+                switch (eldest)
+                {
+                    case "Nicole":
+                    case "nicole":
+                        Console.WriteLine("You guessed Nicole, try again.");
+                        Console.WriteLine("Who is the eldest sister? Nicole, Shauna or Shannon:");
+                        eldest = Console.ReadLine();
+                        break;
+                    case "Shauna":
+                    case "shauna":
+                        Console.WriteLine("You guessed right! Shauna is the eldest.");
+                        isGuessed = true;
+                        break;
+                    case "Shannon":
+                    case "shannon":
+                        Console.WriteLine("You guessed Shannon, try again.");
+                        Console.WriteLine("Who is the eldest sister? Nicole, Shauna or Shannon:");
+                        eldest = Console.ReadLine();
+                        break;
+                    default:
+                        Console.WriteLine("Please pick one of the names provided.");
+                        Console.WriteLine("Who is the eldest sister? Nicole, Shauna or Shannon:");
+                        eldest = Console.ReadLine();
+                        break;
+                }
+            }
+            while (!isGuessed);
+            
+            Console.Read();
+
+        }
+    }
+}
