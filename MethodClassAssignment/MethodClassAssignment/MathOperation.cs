ad7aa5dc182e0f8b43e7707c887ecd48b9929794
Nicole Mongan <97134387+NikiM95@users.noreply.github.com>
2024-04-08 06:20:42 +0100

    Add files via upload

---
+++ MethodClassAssignment/MethodClassAssignment/MathOperation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    public class MathOperation
+    {
+        public void PerformOperation(int num1, int num2)
+        {
+            // Perform a math operation on num1
+            int result = num1 + 10;
+
+            // Display num2 to the screen
+            Console.WriteLine("Second integer is: " + num2);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Instantiate the MathOperation class
+            MathOperation mathOperation = new MathOperation();
+
+            // Call the PerformOperation method passing in two numbers
+            mathOperation.PerformOperation(5, 20);
+
+
+            // Call the PerformOperation method specifying parameters by name
+            mathOperation.PerformOperation(num1: 7, num2: 30);
+
+        }
+    }
+}
