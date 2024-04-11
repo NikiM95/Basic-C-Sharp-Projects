858004760edbc9075201b0dbf38f4774d846b7bc
Nicole Mongan <97134387+NikiM95@users.noreply.github.com>
2024-04-11 02:07:51 +0100

    Add files via upload

---
+++ CodeFirstAssignment/CodeFirstAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Nicole" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+            Console.WriteLine("End");
+            Console.ReadLine();
+        }
+    }
+}
